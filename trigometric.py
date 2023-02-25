d5b7a8e0f340bf484bdd1b2441852830f54b999b
saranshjaiswal426 <Co.2019.sdjaiswal@bitwardha.ac.in>
2023-02-25 16:57:55 +0530

    try trigometric

---
+++ trigometric.py
@@ -0,0 +1,13 @@
+a, b = 1, 2
+import math
+math.sin(a) # returns the sine of 'a' in radians
+# Out: 0.8414709848078965
+print(math.sin(a))
+math.cosh(b) 
+print(math.cosh(b))
+
+math.atan(math.pi)
+print(math.atan(math.pi)) 
+
+math.hypot(a, b)
+print(math.hypot(a, b))
