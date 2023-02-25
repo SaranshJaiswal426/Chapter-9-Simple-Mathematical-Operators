82b208876e68d702e026ef13fd96d2b9c490ded5
saranshjaiswal426 <Co.2019.sdjaiswal@bitwardha.ac.in>
2023-02-25 16:53:03 +0530

    try addition

--- Addition.py
+++ Addition.py
@@ -1,7 +1,8 @@
 a, b = 1, 2
 print(a + b) 
 
-'''a += b 
+a += b 
 import operator
+a, b = 1, 2
 operator.add(a, b) 
-print(a,b)'''#problem in operater import 
+print(operator.add(a, b) )
---
+++ exporential.py
@@ -0,0 +1,6 @@
+a, b = 2, 3
+print((a ** b)) # = 8
+pow(a, b) # = 8
+import math
+math.pow(a, b) 
+print(math.pow(a, b))
